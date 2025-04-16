e18e548bcef7447b7f5a0ffc4b4a401ab9b27d6c
geoprepod <maksikbet13@gmail.com>
2025-04-16 16:30:43 +0300

    first commit

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from math import log2, ceil
+def f(n):
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return 0
+    return 1
 
-N1 = 2 ** 24
-i1 = ceil(log2(N1))
-I0 = 18 * 1024 * 1024 * 8
-K1 = I0 / i1
-N2 = 2 ** 16
-i2 = ceil(log2(N2))
-K2 = K1 / 2
-print(K2 * i2 / 8 / 1024 / 1024)
+
+def fn(n):
+    for i in range(2, int(n ** 0.5) + 1):
+        a = f(i)
+        if n % i == 0 and i > 100 and a:
+            return i
+        elif n % i == 0 and i < 100 and a:
+            return 0
+
+
+for i in range(20222022, 2022, -1):
+    a = fn(i)
+    if not f(i) and a:
+        print(i, a)
