fe3ab0890b356c9a451f9a76e52722fcb43bfbbe
geoprepod <maksikbet13@gmail.com>
2025-04-22 10:17:13 +0300

    goaat

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-def f(n):
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return 0
-    return 1
+for A in range(7000, 1, -1):
+    Flag = True
+    for x in range(300):
+        Flag2 = True
+        for y in range(300):
+            a = (y - x ** 2) != 80
+            b = A < (13 * x - 14)
+            c = A < (y ** 2 + 15)
+            if not (a or b or c):
+                Flag = False
+                break
+        if not Flag2:
+            break
+    if Flag:
+        print(A)
+        break
 
-
-def fn(n):
-    for i in range(2, int(n ** 0.5) + 1):
-        a = f(i)
-        if n % i == 0 and i > 100 and a:
-            return i
-        elif n % i == 0 and i < 100 and a:
-            return 0
-
-
-for i in range(20222022, 2022, -1):
-    a = fn(i)
-    if not f(i) and a:
-        print(i, a)
