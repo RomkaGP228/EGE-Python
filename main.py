dca7714a4815cca38b21cee06896a9ebd3b71913
geo_prepod_mac <romkagp@yandex.ru>
2025-06-07 01:50:18 +0300

    from mac

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-def f(n, base):
-    fin = ''
-    numeric = '0123456789'
-    while n > 0:
-        fin = numeric[n % base] + fin
-        n //= base
-    return fin
-
-
 count = 0
-for i in range(8 ** 6, 8 ** 7):
-    si = str(f(i, 8))
-    if len(set(si)) == len(si) and len(si) == 7:
-        Flag = True
-        for j in range(len(si) - 1):
-            if (int(si[j]) % 2 == 0 and int(si[j + 1]) % 2 == 0) or \
-                    (int(si[j]) % 2 != 0 and int(si[j + 1]) % 2 != 0):
-                Flag = False
-        if Flag:
-            count += 1
+
+for x in range(-5000, 5000):
+    A = 0
+    P = x in [2, 7, 14, 28, 34, 102]
+    Q = x in [7, 12, 24, 28, 56, 94]
+    f = A or ((not P) <= (not Q))
+    if not f:
+        count += x
 print(count)
